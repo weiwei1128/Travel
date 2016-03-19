4ba4bf20b2f10c395fa1ed14187d398d99093344
tinghua81 <as1014513@gmail.com>
2016-03-20 03:12:24 +0800

    旅遊日誌
    1.從DB讀取並顯示
    2.有BUG要修→滑動會重複新增圖片

--- app/src/main/java/com/travel/Adapter/RecordMemoAdapter.java
+++ app/src/main/java/com/travel/Adapter/RecordMemoAdapter.java
@@ -5,24 +5,27 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.Gallery;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.travel.R;
 import com.travel.Utility.DataBaseHelper;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tinghua on 2016/3/3.
@@ -39,6 +42,12 @@ public class RecordMemoAdapter extends BaseAdapter {
     private Context context;
     private DataBaseHelper helper;
     private SQLiteDatabase database;
+
+    private Integer RouteConter = 1;
+
+    private ArrayList<Bitmap> bitmapArrayList = new ArrayList<Bitmap>();
+    private List<ArrayList<Bitmap>> listOfBitmapArray = new ArrayList<ArrayList<Bitmap>>();
+    private int mDotsCount;
 
     public RecordMemoAdapter(Context mcontext) {
         this.context = mcontext;
@@ -87,12 +96,10 @@ public class RecordMemoAdapter extends BaseAdapter {
             if (trackRoute_cursor.getCount() != 0) {
                 trackRoute_cursor.moveToLast();
                 number = trackRoute_cursor.getInt(0);
+                Log.e("3/18_", "rs count:" + number);
             }
             trackRoute_cursor.close();
         }
-        database.close();
-        helper.close();
-
         return number;
     }
 
@@ -111,9 +118,11 @@ public class RecordMemoAdapter extends BaseAdapter {
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.record_memo_list,parent,false);
             mViewHolder = new ViewHolder();
-            mViewHolder.MemoImg = (ImageView) convertView.findViewById(R.id.MemoImageView);
+            mViewHolder.gallery = (Gallery) convertView.findViewById(R.id.gallery);
+            mViewHolder.mDotsLayout = (LinearLayout) convertView.findViewById(R.id.image_count);
+            //mViewHolder.MemoImg = (ImageView) convertView.findViewById(R.id.MemoImageView);
             mViewHolder.MemoTitle = (TextView) convertView.findViewById(R.id.Title);
-            mViewHolder.MemoTimeStamp = (TextView) convertView.findViewById(R.id.TimeStamp);
+            mViewHolder.MemoTotalTime = (TextView) convertView.findViewById(R.id.TimeStamp);
             mViewHolder.MemoString = (TextView) convertView.findViewById(R.id.MemoString);
 
             convertView.setTag(mViewHolder);
@@ -122,36 +131,111 @@ public class RecordMemoAdapter extends BaseAdapter {
             mViewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context).build();
-        ImageLoader.getInstance().init(configuration);
+        //ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context).build();
+        //ImageLoader.getInstance().init(configuration);
 
-        Cursor travelMemo_cursor = database.query("travelmemo", new String[]{"memo_no",
-                        "memo_title", "memo_content", "memo_img", "memo_latlng", "memo_time"},
-                null, null, null, null, null);
-        if (travelMemo_cursor != null && travelMemo_cursor.getCount() > 0) {
-            travelMemo_cursor.moveToPosition(position);
-            mViewHolder.MemoTitle.setText(travelMemo_cursor.getString(1));
-            mViewHolder.MemoString.setText(travelMemo_cursor.getString(2));
-            // TODO 資料庫讀Bitmap
-            byte[] d = travelMemo_cursor.getBlob(3);
-            Bitmap bmp = BitmapFactory.decodeByteArray(d, 0, d.length);
-            //dialog_img.setImageBitmap(bmp);
-            loader.displayImage(travelMemo_cursor.getString(3), mViewHolder.MemoImg, options, listener);
-            mViewHolder.MemoTimeStamp.setText(travelMemo_cursor.getString(5));
-            Log.d("3/13_", "img_url:" + travelMemo_cursor.getString(3));
+        Cursor trackRoute_cursor = database.query("trackRoute",
+                new String[]{"routesCounter", "track_no", "track_lat", "track_lng",
+                        "track_start", "track_title", "track_totaltime", "track_completetime"},
+                "track_start=\"0\"", null, null, null, null, null);
+        if (trackRoute_cursor != null) {
+            if (trackRoute_cursor.getCount() != 0) {
+                trackRoute_cursor.moveToPosition(trackRoute_cursor.getCount()-position-1);
+                mViewHolder.MemoTitle.setText(trackRoute_cursor.getString(5));
+                mViewHolder.MemoTotalTime.setText(trackRoute_cursor.getString(6));
+                RouteConter = trackRoute_cursor.getInt(0);
+
+                Cursor memo_cursor = database.query("travelmemo", new String[]{"memo_routesCounter", "memo_trackNo",
+                                "memo_content", "memo_img", "memo_latlng", "memo_time"},
+                        "memo_routesCounter=\"" + RouteConter + "\"", null, null, null, null, null);
+                if (memo_cursor != null) {
+                    if (memo_cursor.getCount() != 0) {
+                        memo_cursor.moveToFirst();
+                        mViewHolder.MemoString.setText(memo_cursor.getString(2));
+                    } else {
+                        mViewHolder.MemoString.setText("");
+                    }
+                }
+
+                // TODO need to modify 二微陣列
+                Cursor img_cursor = database.query("travelmemo", new String[]{"memo_routesCounter", "memo_trackNo",
+                                "memo_content", "memo_img", "memo_latlng", "memo_time"},
+                        "memo_routesCounter=\"" + RouteConter + "\" AND memo_img!=\"null\"", null, null, null, null, null);
+                if (img_cursor != null) {
+                    int number;
+                    if (img_cursor.getCount() != 0) {
+                        number = img_cursor.getCount();
+                        Log.e("3/18_", "img count:" + number);
+                        bitmapArrayList.clear();
+                        while (img_cursor.moveToNext()) {
+                            Log.e("3/18_", "img: " + img_cursor.getBlob(3));
+                            byte[] d = img_cursor.getBlob(3);
+                            bitmapArrayList.add(BitmapFactory.decodeByteArray(d, 0, d.length));
+                        }
+                        // TODO 前後滑動會重複新增!
+                        //if (listOfBitmapArray.size() != 0)
+                            //listOfBitmapArray.get(position).clear();
+                        listOfBitmapArray.add(position, bitmapArrayList);
+                    } else {
+                        Log.e("3/18_", "img_cursor = 0");
+                    }
+                    img_cursor.close();
+                } else {
+                    Log.e("3/18_", "img_cursor = null");
+                }
+
+                mViewHolder.gallery.setAdapter(new MemoImageSlideAdapter(context, position, listOfBitmapArray));
+                mDotsCount = mViewHolder.gallery.getAdapter().getCount();
+                mViewHolder.mDotsText = new TextView[mDotsCount];
+
+                for (int i = 0; i < mDotsCount; i++) {
+                    mViewHolder.mDotsText[i] = new TextView(context);
+                    mViewHolder.mDotsText[i].setText(".");
+                    mViewHolder.mDotsText[i].setTextSize(45);
+                    mViewHolder.mDotsText[i].setTypeface(null, Typeface.BOLD);
+                    mViewHolder.mDotsText[i].setTextColor(android.graphics.Color.GRAY);
+                    mViewHolder.mDotsLayout.addView(mViewHolder.mDotsText[i]);
+                }
+
+                mViewHolder.gallery.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView adapterView, View view, int pos, long l) {
+
+                        for (int i = 0; i < mDotsCount; i++) {
+                            mViewHolder.mDotsText[i].setTextColor(Color.GRAY);
+                        }
+
+                        mViewHolder.mDotsText[pos].setTextColor(Color.WHITE);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView adapterView) {
+
+                    }
+                });
+
+                if (memo_cursor != null)
+                    memo_cursor.close();
+
+                if (img_cursor != null)
+                    img_cursor.close();
+            }
         }
 
+/*
         if (mViewHolder.MemoImg != null)
             mViewHolder.MemoImg.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-        if (travelMemo_cursor != null)
-            travelMemo_cursor.close();
+*/
+        if (trackRoute_cursor != null)
+            trackRoute_cursor.close();
 
         return convertView;
     }
 
     private static class ViewHolder {
-        ImageView MemoImg;
-        TextView MemoTitle, MemoTimeStamp, MemoString;
+        Gallery gallery;
+        //ImageView MemoImg;
+        TextView mDotsText[], MemoTitle, MemoTotalTime, MemoString;
+        LinearLayout mDotsLayout;
     }
 }
